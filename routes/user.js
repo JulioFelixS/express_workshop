5ffd0c8b5b196767ae244fe2c0ffa1c25a4b11ae
JulioFelixS <72428216+JulioFelixS@users.noreply.github.com>
2020-11-05 12:40:48 -0600

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const user = express.Router();
+const jwt = require('jsonwebtoken');
 const db = require('../config/database');
 
 user.post("/", async(req, res, next) => {
@@ -23,9 +24,21 @@ user.post("/login", async (req, res, next) => {
 	const query = `SELECT * FROM user WHERE user_mail = ' ${user_mail} 'AND user_password = '${user_password} ';`;
 	const rows = await db.query(query);
 
-	console.log(rows);
+	
+	if (user_mail && user_password ) {
+		if (rows.length == 1){
+			const token = jwt.sign({
+				user_id: rows[0].user_id,
+				user_mail: rows[0].user_mail
 
-	return res.status(200).json({code: 200, message: ""});
+			}, "debugkey");
+			return res.status(200).json({code: 200, message: token});
+		}
+		else{
+			return res.status(401).json({code:401, message:"Usuario y/o contraseña incorrecta"});
+		}
+	}
+	return res.status(500).json({code: 500, message: "Datos incompletos"});
 });
 
 user.get("/", async (req, res, next) => {
