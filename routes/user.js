8270d411190de05f23a759a7b2e5de4f32ee54d1
JulioFelixS <72428216+JulioFelixS@users.noreply.github.com>
2020-11-04 23:55:07 -0600

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ user.post("/", async(req, res, next) => {
 	
 	if(user_name && user_mail && user_password){
 		let query = "INSERT INTO user (user_name, user_mail, user_password)"; 
-		query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`;
+		query += `VALUES ('${user_name}', '${user_mail}', '${user_password}');`;
 		const rows = await db.query(query);
 
 		if(rows.affectedRows == 1){
@@ -16,6 +16,16 @@ user.post("/", async(req, res, next) => {
 		return res.status(500).json({code: 500, message: "Ocurrio un error"});
 	}
 	return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
+user.post("/login", async (req, res, next) => {
+	const { user_mail, user_password} = req.body;
+	const query = `SELECT * FROM user WHERE user_mail = ' ${user_mail} 'AND user_password = '${user_password} ';`;
+	const rows = await db.query(query);
+
+	console.log(rows);
+
+	return res.status(200).json({code: 200, message: ""});
 });
 
 user.get("/", async (req, res, next) => {
